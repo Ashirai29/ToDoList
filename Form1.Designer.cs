58ff1a06d9c8dd0f9fa7de3e3dca9b96b712cb08
Ashirai29 <AAHLATSHWAYO10@GMAIL.COM>
2025-08-12 17:33:32 +0200

    Initial Commit.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ToDoList
 {
-    partial class Form1
+    partial class cfrmToDoList
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,181 @@ namespace ToDoList
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpSunday = new System.Windows.Forms.TabPage();
+            this.tpMonday = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tpTuesday = new System.Windows.Forms.TabPage();
+            this.tpWednesday = new System.Windows.Forms.TabPage();
+            this.tpThursday = new System.Windows.Forms.TabPage();
+            this.tpFriday = new System.Windows.Forms.TabPage();
+            this.tpSaturday = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpSunday);
+            this.tabControl1.Controls.Add(this.tpMonday);
+            this.tabControl1.Controls.Add(this.tpTuesday);
+            this.tabControl1.Controls.Add(this.tpWednesday);
+            this.tabControl1.Controls.Add(this.tpThursday);
+            this.tabControl1.Controls.Add(this.tpFriday);
+            this.tabControl1.Controls.Add(this.tpSaturday);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(721, 479);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tpSunday
+            // 
+            this.tpSunday.Location = new System.Drawing.Point(4, 25);
+            this.tpSunday.Name = "tpSunday";
+            this.tpSunday.Padding = new System.Windows.Forms.Padding(3);
+            this.tpSunday.Size = new System.Drawing.Size(713, 450);
+            this.tpSunday.TabIndex = 0;
+            this.tpSunday.Text = "Sunday";
+            this.tpSunday.UseVisualStyleBackColor = true;
+            // 
+            // tpMonday
+            // 
+            this.tpMonday.Location = new System.Drawing.Point(4, 25);
+            this.tpMonday.Name = "tpMonday";
+            this.tpMonday.Padding = new System.Windows.Forms.Padding(3);
+            this.tpMonday.Size = new System.Drawing.Size(713, 450);
+            this.tpMonday.TabIndex = 1;
+            this.tpMonday.Text = "Monday";
+            this.tpMonday.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(753, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add new task";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tpTuesday
+            // 
+            this.tpTuesday.Location = new System.Drawing.Point(4, 25);
+            this.tpTuesday.Name = "tpTuesday";
+            this.tpTuesday.Size = new System.Drawing.Size(713, 450);
+            this.tpTuesday.TabIndex = 2;
+            this.tpTuesday.Text = "Tuesday";
+            this.tpTuesday.UseVisualStyleBackColor = true;
+            // 
+            // tpWednesday
+            // 
+            this.tpWednesday.Location = new System.Drawing.Point(4, 25);
+            this.tpWednesday.Name = "tpWednesday";
+            this.tpWednesday.Size = new System.Drawing.Size(713, 450);
+            this.tpWednesday.TabIndex = 3;
+            this.tpWednesday.Text = "Wednesday";
+            this.tpWednesday.UseVisualStyleBackColor = true;
+            // 
+            // tpThursday
+            // 
+            this.tpThursday.Location = new System.Drawing.Point(4, 25);
+            this.tpThursday.Name = "tpThursday";
+            this.tpThursday.Size = new System.Drawing.Size(713, 450);
+            this.tpThursday.TabIndex = 4;
+            this.tpThursday.Text = "Thursday";
+            this.tpThursday.UseVisualStyleBackColor = true;
+            // 
+            // tpFriday
+            // 
+            this.tpFriday.Location = new System.Drawing.Point(4, 25);
+            this.tpFriday.Name = "tpFriday";
+            this.tpFriday.Size = new System.Drawing.Size(713, 450);
+            this.tpFriday.TabIndex = 5;
+            this.tpFriday.Text = "Friday";
+            this.tpFriday.UseVisualStyleBackColor = true;
+            // 
+            // tpSaturday
+            // 
+            this.tpSaturday.Location = new System.Drawing.Point(4, 25);
+            this.tpSaturday.Name = "tpSaturday";
+            this.tpSaturday.Size = new System.Drawing.Size(713, 450);
+            this.tpSaturday.TabIndex = 6;
+            this.tpSaturday.Text = "Saturday";
+            this.tpSaturday.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(753, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(753, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(753, 238);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(753, 303);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // cfrmToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(933, 570);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "cfrmToDoList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "What To DO";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tpSunday;
+        private System.Windows.Forms.TabPage tpMonday;
+        private System.Windows.Forms.TabPage tpTuesday;
+        private System.Windows.Forms.TabPage tpWednesday;
+        private System.Windows.Forms.TabPage tpThursday;
+        private System.Windows.Forms.TabPage tpFriday;
+        private System.Windows.Forms.TabPage tpSaturday;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ToDoList
 {
-    public partial class Form1 : Form
+    public partial class cfrmToDoList : Form
     {
-        public Form1()
+        public cfrmToDoList()
         {
             InitializeComponent();
         }
